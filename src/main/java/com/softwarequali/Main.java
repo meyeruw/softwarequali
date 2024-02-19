829029959c589bf578aacc0bdd0d6ae69338ac23
MarcScheller <100428557+MarcScheller@users.noreply.github.com>
2024-02-19 13:49:46 +0100

    Added value ranges

--- src/main/java/com/softwarequali/Main.java
+++ src/main/java/com/softwarequali/Main.java
@@ -1,9 +1,16 @@
 package com.softwarequali;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
     private int pressure;
     private boolean acousticSignalTriggered;
     private boolean managerNotified;
+    private boolean informedMaintenanceTeam;
+    private boolean alarmTriggered;
+    private boolean evacuation;
 
     public void setPressure(int pressure) {
         this.pressure = pressure;
@@ -19,14 +26,55 @@ public class Main {
     }
 
     private void checkPressure() {
+
+        // Critical minimum value
         if (pressure < 50) {
-            // Critical minimum value reached
             acousticSignalTriggered = true;
             managerNotified = true;
-        } else {
-            // Resetting states
+            informedMaintenanceTeam = false;
+            alarmTriggered = false;
+            evacuation = false;
+
+            // Critical maximum value
+        } else if (pressure > 300) {
+            acousticSignalTriggered = true;
+            managerNotified = true;
+            informedMaintenanceTeam = true;
+            alarmTriggered = false;
+            evacuation = false;
+
+            // Optimal value
+        } else if (pressure > 180 && pressure < 220) {
             acousticSignalTriggered = false;
             managerNotified = false;
+            informedMaintenanceTeam = false;
+            alarmTriggered = false;
+            evacuation = false;
+
+            // Underpressure
+        } else if (pressure >= 50 && pressure <= 180) {
+            acousticSignalTriggered = false;
+            managerNotified = true;
+            informedMaintenanceTeam = true;
+            alarmTriggered = false;
+            evacuation = false;
+
+            // Overpressure
+        } else if (pressure >= 220 && pressure <= 300) {
+            acousticSignalTriggered = false;
+            managerNotified = true;
+            informedMaintenanceTeam = true;
+            alarmTriggered = false;
+            evacuation = false;
+
+            // Dangerous value
+        } else if (pressure > 500) {
+            acousticSignalTriggered = false;
+            managerNotified = false;
+            informedMaintenanceTeam = false;
+            alarmTriggered = true;
+            evacuation = true;
+
         }
     }
 }

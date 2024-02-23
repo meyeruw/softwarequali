70d4b0b9653c1458a56e9aacdc63e8094f2a318c
Crowfort <meyeruw@dhbw-loerrach.de>
2024-02-23 02:55:50 +0100

    add logserver response

--- src/main/java/com/softwarequali/LogSenderMock.java
+++ src/main/java/com/softwarequali/LogSenderMock.java
@@ -5,8 +5,19 @@ public class LogSenderMock {
     private boolean wasLogSent = false;
 
     public void sendLog(String logMessage) {
-        wasLogSent = true;
-        System.out.println("Log message sent to Log Server: " + logMessage);
+        boolean serverResponse = simulateServerResponse(logMessage);
+        if (serverResponse) {
+            wasLogSent = true;
+            System.out.println("Log message sent to Log Server: " + logMessage);
+        } else {
+            wasLogSent = false;
+            System.out.println("Failed to send log message to Log Server: " + logMessage);
+        }
+    }
+
+    private boolean simulateServerResponse(String logMessage) {
+        // Simulate a server response.
+        return logMessage.length() > 0;
     }
 
     public boolean wasLogSent() {
